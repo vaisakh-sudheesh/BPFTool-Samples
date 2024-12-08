e7ab4e02c17a16f8eed3ba68b5935b4b99ea30e9
Vaisakh P S <vaisakh.sudheesh@gmail.com>
2024-12-09 03:20:58 +0530

    Dependencies built from Pixel-9(Tokay) mainline code for bpftool

--- deps/bpftool-android/android-bpftool-build.sh
+++ deps/bpftool-android/android-bpftool-build.sh
@@ -12,6 +12,8 @@ source build/envsetup.sh
 lunch aosp_arm64-trunk_staging-userdebug
 mma -j12  bpftool libbpf libelf libz_static libzstd
 
-find -D exec out/target/product/generic_arm64/obj/ \(-name libzstd.a -o -name libbpf.a -o -name libelf.a -o -name libz_static.a -o -name libzstd.a \) -print0 | xargs -0 -I {} cp -f {} $PROJECT_PATH/libs/
+find -D exec out/target/product/generic_arm64/obj/ \( -name libzstd.a -o -name libbpf.a -o -name libelf.a -o -name libz_static.a -o -name libzstd.a \) -print0 | xargs -0 -I {} cp -f {} $PROJECT_PATH/libs/
 
-find -D exec out/target/product/generic_arm64/obj/ \(-name bpftool \) -print0 | xargs -0 -I {} cp -f {} $PROJECT_PATH/bin/
+find -D exec out/target/product/generic_arm64/obj/ \( -name bpftool \) -print0 | xargs -0 -I {} cp -f {} $PROJECT_PATH/bin/
+
+cp -avr out/soong/.intermediates/external/libbpf/libbpf_headers/gen/ $PROJECT_PATH/include/
