375abfd398beb5f7cfc27fb1031ef05e4c6f1813
Vaisakh P S <vaisakh.sudheesh@gmail.com>
2024-12-09 04:53:15 +0530

    Handle missed arch specification for BPF in generate_build_dir_header

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -5,6 +5,7 @@ function(generate_build_dir_header output_file)
     file(WRITE ${output_file} "#ifndef BUILD_DIR_H\n")
     file(APPEND ${output_file} "#define BUILD_DIR_H\n\n")
     file(APPEND ${output_file} "#define BUILD_DIR \"${CMAKE_BINARY_DIR}/lib\"\n\n")
+    file(APPEND ${output_file} "#define __TARGET_ARCH_x86\n\n")
     file(APPEND ${output_file} "#endif // BUILD_DIR_H\n")
 endfunction()
 
